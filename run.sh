561c3e9ccce1408019e72fc4ee501473e0950a70
Nicolargo <nicolas@nicolargo.com>
2017-02-06 15:24:02 +0100

    Update for latest InfluxDB version

--- run.sh
+++ run.sh
@@ -2,5 +2,12 @@
 
 docker-compose up -d
 
-echo "Grafana: http://localhost:3000 - admin/admin"
-echo "InfluxDB: http://localhost:8083 - root/root"
+echo "Grafana: http://127.0.0.1:3000 - admin/admin"
+
+echo
+echo "Current database list"
+curl -G http://localhost:8086/query?pretty=true --data-urlencode "db=glances" --data-urlencode "q=SHOW DATABASES"
+
+echo
+echo "Create a new database ?"
+echo "curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=CREATE DATABASE mydb'"
